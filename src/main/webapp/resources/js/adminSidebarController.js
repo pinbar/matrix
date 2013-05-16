ab41f415a9740cc7d604edbfde2dde2b679e2141
Amit Pant <amit@percipientedge.com>
2013-05-16 00:03:17 -0400

    Fixed another merge conflict

--- src/main/webapp/resources/js/adminSidebarController.js
+++ src/main/webapp/resources/js/adminSidebarController.js
@@ -13,96 +13,126 @@ var adminSidebarController = function() {
         clientAddLinkClicked : "a#clientadd"
     },
 
-        _divSelectors = [ "div#adminMsgs", "div#grpList", "div#grpUpdate",
+    _divSelectors = [ "div#adminMsgs", "div#grpList", "div#grpUpdate",
             "div#empList", "div#empUpdate", "div#costCenterList",
-            "div#costCenterUpdate", "div#empResetPassword", "div#clientUpdate", "div#clientList"  ],
+            "div#costCenterUpdate", "div#empResetPassword", "div#clientUpdate",
+            "div#clientList" ],
 
-        _showHideForms = function(formToShow) {
-            $.each(_divSelectors, function(index, data) {
-                if (data !== formToShow) {
-                    $(data).addClass('hide');
-                } else {
-                    $(data).removeClass('hide');
-                }
-            });
-        },
+    _showHideForms = function(formToShow) {
+        $.each(_divSelectors, function(index, data) {
+            if (data !== formToShow) {
+                $(data).addClass('hide');
+            } else {
+                $(data).removeClass('hide');
+            }
+        });
+    },
 
-        _onCostcenterAdd = function(e) {
-            populateOptions({selectedName:"Internal",alwaysShow:true,url:"/admin/client/listAsJson",optionsContainer:"#clientName"});//set 'Internal' by default
+    _onCostcenterAdd = function(e) {
+        populateOptions({
+            selectedName : "Internal",
+            alwaysShow : true,
+            url : "/admin/client/listAsJson",
+            optionsContainer : "#clientName"
+        });// set
+        // 'Internal'
+        // by
+        // default
         $(".text-error").addClass('hide');
         _showHideForms("div#costCenterUpdate");
         $("form#costCenterForm").find("input[type=text], textarea").val("");
     },
 
-        _onCostcenterList = function(e) {
+    _onCostcenterList = function(e) {
         _showHideForms("div#costCenterList");
     },
 
-        _onEmpAdd = function(e) {
-        populateOptions({selectedName:"Employees", alwaysShow:true,url:"/admin/group/listAsJson",optionsContainer:"#groupName"});//set 'Employees' by default
-        populateOptions({selectedName:"Holiday", alwaysShow:true, url:"/admin/costCenter/listAsJson",optionsContainer:".costCode"});//set 'Employees' by default
+    _onEmpAdd = function(e) {
+        populateOptions({
+            selectedName : "Employees",
+            alwaysShow : true,
+            url : "/admin/group/listAsJson",
+            optionsContainer : "#groupName"
+        });// set
+        // 'Employees'
+        // by
+        // default
+        populateOptions({
+            selectedName : "Holiday",
+            alwaysShow : true,
+            url : "/admin/costCenter/listAsJson",
+            optionsContainer : ".costCode"
+        });// set
+        // 'Employees'
+        // by
+        // default
         $(".text-error").addClass('hide');
         _showHideForms("div#empUpdate");
-        $("form#employeeForm").find("input, textarea").not(':button, :submit, :reset').val("");
+        $("form#employeeForm").find("input, textarea").not(
+                ':button, :submit, :reset').val("");
     },
 
-        _onEmpList = function(e) {
+    _onEmpList = function(e) {
         _showHideForms("div#empList");
     },
 
-        _onGrpAdd = function(e) {
+    _onGrpAdd = function(e) {
         $(".text-error").addClass('hide');
         _showHideForms("div#grpUpdate");
         $("form#groupForm").find("input[type=text], textarea").val("");
     },
 
-        _onGrpList = function(e) {
+    _onGrpList = function(e) {
         _showHideForms("div#grpList");
     },
 
-        _onAdminHome = function(e) {
+    _onAdminHome = function(e) {
         _showHideForms("div#adminMsgs");
     },
-    
-        _onClientAdd = function(e) {
+
+    _onClientAdd = function(e) {
         $(".text-error").addClass('hide');
         _showHideForms("div#clientUpdate");
-        $("form#clientForm").find("input, textarea").not(':button, :submit, :reset').val("");
+        $("form#clientForm").find("input, textarea").not(
+                ':button, :submit, :reset').val("");
     },
 
-        _onClientList = function(e) {
+    _onClientList = function(e) {
         _showHideForms("div#clientList");
     },
-    
-    populateOptions= function(args){
-        var show = args.alwaysShow
-        || !($(args.container).hasClass("hide"));
+
+    populateOptions = function(args) {
+        var show = args.alwaysShow || !($(args.container).hasClass("hide"));
         if (show) {
-            $.ajax({
-                url : contextPath + args.url,
-                type : "GET",
-                dataType : "json",
-                success : function(response, textStatus, jqXHR) {
-                    var html = "";
-                    $.each(
-                                    response,
-                                    function(key, value) {
-                                        var grpName = value.name;
-                                        html = html
-                                                + "<option value=\""
-                                                + grpName
-                                                + "\""
-                                                + (args.selectedName
-                                                        && grpName === args.selectedName ? " selected "
-                                                        : "") + " >"
-                                                + grpName + "</option>";
-                                    });
-                    $(args.optionsContainer).empty().append(html);
-                },
-                error : function(response, textStatus, jqXHR) {
-                    alert("error");
-                }
-            });  
+            $
+                    .ajax({
+                        url : contextPath + args.url,
+                        type : "GET",
+                        dataType : "json",
+                        success : function(response, textStatus, jqXHR) {
+                            var html = "";
+                            $
+                                    .each(
+                                            response,
+                                            function(key, value) {
+                                                var grpName = value.name;
+                                                html = html
+                                                        + "<option value=\""
+                                                        + grpName
+                                                        + "\""
+                                                        + (args.selectedName
+                                                                && grpName === args.selectedName ? " selected "
+                                                                : "") + " >"
+                                                        + grpName + "</option>";
+                                            });
+                            $(args.optionsContainer).empty().append(html);
+                        },
+                        error : function(response, textStatus, jqXHR) {
+                            alert("error");
+                        }
+                    });
+        }
+    }
     return ({
         costcenterAddLinkClicked : _onCostcenterAdd,
         costcenterListLinkClicked : _onCostcenterList,
